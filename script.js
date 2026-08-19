88d06704444822d409cca5319255dc3e7b091743
JazeelMuhammedC <cjazeel@gmail.com>
2026-08-19 21:25:03 +0530

    Add javascript to profile

---
+++ script.js
@@ -0,0 +1,12 @@
+const title = document.getElementById("welcome-title");
+
+title.textContent = "Welcome To My Portfolio";
+
+const skill = "JavaScript";
+console.log(skill);
+
+const experience = 0;
+console.log(experience);
+
+const goals =document.getElementById("goal");
+goals.textContent = "Be Success";
